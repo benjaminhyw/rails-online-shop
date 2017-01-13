4cde2f6b900bc49a8dff5c2d56fd7419e18d4e48
Ben Flores <benjaminhyw@gmail.com>
2017-01-12 21:32:49 -0800

    Create route for delete /items/:id

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
   get '/items/:id', to: 'items#show', as: 'item'
   get '/items/:id/edit', to: 'items#edit', as: 'edit_item'
   patch '/items/:id', to: 'items#update'
+  delete '/items/:id', to: 'items#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
