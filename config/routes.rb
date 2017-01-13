6678efea75a7a92df1df7e88b14b18498af90ecb
Ben Flores <benjaminhyw@gmail.com>
2017-01-12 19:59:54 -0800

    Give the get /items/id route a name using the as keyword

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get '/items', to: 'items#index'
-  get '/items/:id', to: 'items#show'
+  get '/items/:id', to: 'items#show', as: 'item'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
