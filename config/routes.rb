0a47d4d1c9c601037d0a347d2feb5c57b93b3e49
Ben Flores <benjaminhyw@gmail.com>
2017-01-12 20:14:16 -0800

    Name the get /items/new route "new_item" using the as keyword

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/items', to: 'items#index'
-  get '/items/new', to: 'items#new'
+  get '/items/new', to: 'items#new', as: 'new_item'
   get '/items/:id', to: 'items#show', as: 'item'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
