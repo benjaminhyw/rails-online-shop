be9574b7c7f4042a78d5299deefc243db39ccaad
Ben Flores <benjaminhyw@gmail.com>
2017-01-13 15:03:55 -0800

    Write test checking validations for creating a new item work

--- test/models/item_test.rb
+++ test/models/item_test.rb
@@ -4,4 +4,8 @@ class ItemTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "should not save item without name, description, quantity, or price" do
+    item = Item.new
+    assert_not item.save, "Saved the article without one of the required fields"
+  end
 end
