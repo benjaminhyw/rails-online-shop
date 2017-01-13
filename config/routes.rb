895ddc948302bf0b96f76468fa210ffaf7555649
Ben Flores <benjaminhyw@gmail.com>
2017-01-12 21:25:13 -0800

    Name get /items/:id/edit using as keyword

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/items/new', to: 'items#new', as: 'new_item'
   post '/items', to: 'items#create'
   get '/items/:id', to: 'items#show', as: 'item'
-  get '/items/:id/edit', to: 'items#edit'
+  get '/items/:id/edit', to: 'items#edit', as: 'edit_item'
   patch '/items/:id', to: 'items#update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
