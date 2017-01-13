07559633df360dd07f7cb4badc816c0e3529d6dd
Ben Flores <benjaminhyw@gmail.com>
2017-01-12 20:22:45 -0800

    Create post route for /items to allow for form post submissions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get '/items', to: 'items#index'
   get '/items/new', to: 'items#new', as: 'new_item'
+  post '/items', to: 'items#create'
   get '/items/:id', to: 'items#show', as: 'item'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
