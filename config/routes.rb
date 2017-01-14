b797b25800b6fec8f38c373d79ec54e337290ce8
Ben Flores <benjaminhyw@gmail.com>
2017-01-14 14:30:59 -0800

    Create new route for new users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'items#index'
 
+  get '/users/new', to: 'users#new'
+
   get '/items', to: 'items#index'
   get '/items/new', to: 'items#new', as: 'new_item'
   post '/items', to: 'items#create'
@@ -8,6 +10,5 @@ Rails.application.routes.draw do
   get '/items/:id/edit', to: 'items#edit', as: 'edit_item'
   patch '/items/:id', to: 'items#update'
   delete '/items/:id', to: 'items#destroy'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
