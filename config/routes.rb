307831bbbc15e0ef319b38c1bafae5ce5f30050a
Ben Flores <benjaminhyw@gmail.com>
2017-01-14 15:58:32 -0800

    Create route for individual user show pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/users', to: 'users#index'
   get '/users/new', to: 'users#new', as: 'new_user'
   post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
 
   get '/items', to: 'items#index'
   get '/items/new', to: 'items#new', as: 'new_item'
