277a17e44f1d496f9b5dad9371df1efdec54c072
Ben Flores <benjaminhyw@gmail.com>
2017-01-14 16:01:10 -0800

    Create empty users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,10 @@ class UsersController < ApplicationController
     @user.save
   end
 
+  def show
+
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
