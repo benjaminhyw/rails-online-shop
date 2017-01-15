7c6e6f1363aa8252e5adf384048736a1b53527c7
Ben Flores <benjaminhyw@gmail.com>
2017-01-14 16:22:22 -0800

    Refactor, create private users#find_user and call it in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,10 +10,14 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    find_user
   end
 
   private
+    def find_user
+      @user = User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:username, :email, :password)
     end
