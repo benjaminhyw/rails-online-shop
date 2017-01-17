71e6eec26528920bc84330448eb26d564f223807
Ben Flores <benjaminhyw@gmail.com>
2017-01-16 18:10:42 -0800

    Create and flesh out users#destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,13 @@ class UsersController < ApplicationController
     redirect_to @user
   end
 
+  def destroy
+    find_user
+    @user.destroy
+
+    redirect_to items_path
+  end
+
   private
     def find_user
       @user = User.find(params[:id])
