4fe98669194366bd1f2edf7be8f10bdd792fdaf8
Ben Flores <benjaminhyw@gmail.com>
2017-01-17 17:43:18 -0800

    Update flash error so it only appears once when the error occured, rather than a persisting message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       #Log the user in and redirect to the user's show page (for now)
       # render new_user_path
     else
-      flash[:danger] = 'Invalid username/password combination'
+      flash.now[:danger] = 'Invalid username/password combination'
       render 'new'
     end
   end
