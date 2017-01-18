738d3622037810f923a781c6866c613715bbd0b4
Ben Flores <benjaminhyw@gmail.com>
2017-01-17 17:41:01 -0800

    Begin to flesh out sessions#create.  Statement works if a user types in incorrect username, but doesnt yet work if the username exists (regardless of correct or incorrect password)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    user = User.find_by(username: params[:session][:username])
+    if user && user.autenticate(params[:session][:password])
+      #Log the user in and redirect to the user's show page (for now)
+      # render new_user_path
+    else
+      flash[:danger] = 'Invalid username/password combination'
+      render 'new'
+    end
+  end
+
+  def destroy
+
   end
 end
