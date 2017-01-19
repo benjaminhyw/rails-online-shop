96da8359b14bd656a5c1f9fee4a42ae2d19b1ce8
Ben Flores <benjaminhyw@gmail.com>
2017-01-18 23:51:12 -0800

    Call the log_in helper method

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: params[:session][:username])
     if user && (user.password == (params[:session][:password]))
       #Log the user in and redirect to the user's show page (for now)
+      log_in user
       redirect_to user_path(user)
     else
       flash.now[:danger] = 'Invalid username/password combination'
