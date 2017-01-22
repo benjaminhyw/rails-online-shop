6f20358dbbb7effb6d0c94fe3a15698a80bdb685
Ben Flores <benjaminhyw@gmail.com>
2017-01-21 16:46:50 -0800

    Update validation to contain an error message

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-  validates :username, presence: true, length: { in: 4..15 }, uniqueness: true
+  validates :username, presence: true, length: { in: 4..15, length_message: 'Username must be between 4-15 characters in length' }, uniqueness: true
+  # email validation still needs email format validation
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { in: 4..10 }
 
