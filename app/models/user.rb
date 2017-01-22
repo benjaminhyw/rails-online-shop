87fc0f60a68f453c077e53d3204ab3cfc3d3e606
Ben Flores <benjaminhyw@gmail.com>
2017-01-21 16:23:57 -0800

    Update user validations so more helpers could be used

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-  validates_presence_of :username, :email, :password
+  validates :username, presence: true, length: { in: 4..15 }, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 4..10 }
+
   has_secure_password
 end
