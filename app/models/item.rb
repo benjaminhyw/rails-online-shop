30f7037c6895e8c60a4eb1b64883304755957c87
Ben Flores <benjaminhyw@gmail.com>
2017-01-22 20:49:31 -0800

    Update Item name validation with multiple keys

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,3 @@
 class Item < ApplicationRecord
-  validates_presence_of :name, :description, :quantity, :price
+  validates :name, presence: { message: "Name field can't be left blank"}, length: { in: 3..64, message: "Name must be between 3 and 64 characters in length" }
 end
