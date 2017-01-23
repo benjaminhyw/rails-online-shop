7ac1e579ab98d650fa3fa438491fad6ae33922c4
Ben Flores <benjaminhyw@gmail.com>
2017-01-22 19:37:20 -0800

    Updat User model Email validation error messages.  Minor update is username validation, better wording

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
-  validates :username, presence: { message: "Username can't be blank"}, length: { in: 4..15, message: 'Username must be between 4-15 characters in length' }, uniqueness: { message: 'Username is already taken'}
+  validates :username, presence: { message: "Username field can't be left blank"}, length: { in: 4..15, message: 'Username must be between 4-15 characters in length' }, uniqueness: { message: 'Username is already taken'}
   # email validation still needs email format validation
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: { message: "Email can't be left blank" }, uniqueness: { message: "Email is already taken"}
   validates :password, presence: true, length: { in: 4..10 }
 
   has_secure_password
