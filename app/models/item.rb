88694f5d178a397c9e9fb1fbc21983c05b676d41
Ben Flores <benjaminhyw@gmail.com>
2017-01-22 20:50:04 -0800

    Update Item description validation with multiple keys

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,4 @@
 class Item < ApplicationRecord
   validates :name, presence: { message: "Name field can't be left blank"}, length: { in: 3..64, message: "Name must be between 3 and 64 characters in length" }
+  validates :description, presence: { message: "Description field can't be left blank" }, length: { in: 3..3000, message: "Description must be between 3 and 3,000  characters" }
 end
