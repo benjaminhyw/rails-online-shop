ce574777851a48aae9bf6b45d52b90840e440034
Ben Flores <benjaminhyw@gmail.com>
2017-01-23 15:49:11 -0800

    Write test for clicking on a new item link

--- test/controllers/items_controller_test.rb
+++ test/controllers/items_controller_test.rb
@@ -4,4 +4,9 @@ class ItemsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get new item form page" do
+    get new_item_path
+    assert_response :success
+  end
 end
