ba50d8c9945e092111e93394372d8b8a79033d15
Ben Flores <benjaminhyw@gmail.com>
2017-01-23 16:09:12 -0800

    Write test for new user registration url

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -4,4 +4,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should get new user registration form page" do
+    get new_user_url
+    assert_response :success
+  end
+
 end
