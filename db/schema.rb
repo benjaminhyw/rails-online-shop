c66b975a2490e055a6bd69c6cef3f00e90ba5537
Ben Flores <benjaminhyw@gmail.com>
2017-01-25 12:03:16 -0800

    Generate a new migration for a remember_digest column within the user table.  This is the first step for a better session involving cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170121025543) do
+ActiveRecord::Schema.define(version: 20170125200052) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
@@ -25,10 +25,12 @@ ActiveRecord::Schema.define(version: 20170121025543) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
+    t.string   "password"
     t.boolean  "admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
   end
 
 end
