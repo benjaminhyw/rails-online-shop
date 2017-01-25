d265b1219213bdad36217021f2ad526c54534a2b
Ben Flores <benjaminhyw@gmail.com>
2017-01-25 12:31:17 -0800

    Create attr_accessor :remember_token to create an accessible attribute

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 require 'bcrypt'
 
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   validates :username, presence: { message: "Username field can't be left blank"}, length: { in: 4..15, message: 'Username must be between 4-15 characters in length' }, uniqueness: { message: 'Username is already taken'}
   # email validation still needs email format validation
   validates :email, presence: { message: "Email can't be left blank" }, uniqueness: { message: "Email is already taken"}
