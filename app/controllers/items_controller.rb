bb53fdceca525ba9849babc46b4e3517e9a67503
Ben Flores <benjaminhyw@gmail.com>
2017-01-27 11:08:27 -0800

    Move location of ItemsController private #find_item and #item_params to ItemsHelper

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -62,13 +62,4 @@ class ItemsController < ApplicationController
       redirect_to root_path
     end
   end
-
-  private
-    def find_item
-      @item = Item.find_by_id(params[:id])
-    end
-
-    def item_params
-      params.require(:item).permit(:name, :description, :image, :price, :quantity )
-    end
 end
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,11 @@
 module ItemsHelper
+  private
+    def find_item
+      @item = Item.find_by_id(params[:id])
+    end
+
+    def item_params
+      params.require(:item).permit(:name, :description, :image, :price, :quantity )
+    end
+
 end
