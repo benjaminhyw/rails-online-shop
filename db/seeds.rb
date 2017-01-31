74aef181d103d4c7387031a9eb5aa503b3186767
Ben Flores <benjaminhyw@gmail.com>
2017-01-31 11:52:59 -0800

    Use the fager gem inside seed file for dummy item data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+25.times do
+  Item.create({
+    name: Faker::StarWars.character,
+    description: Faker::StarWars.quote,
+    image: Faker::LoremPixel.image,
+    quantity: 100,
+    price: Faker::Commerce.price
+  })
+end
