cc912b3155cadd5915c26e5e2077c2432b0d346b
Ben Flores <benjaminhyw@gmail.com>
2017-01-31 14:32:43 -0800

    Update nav background-color change from 50px to 110px trigger

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,12 +18,12 @@
 $(function() {
 
   $(window).on("scroll", function() {
-    if($(window).scrollTop() > 50) {
+    if($(window).scrollTop() > 110) {
         $("#nav").addClass("active");
     } else {
         //remove the background property so it comes transparent again (defined in your css)
        $("#nav").removeClass("active");
     }
   });
-  
+
 });
