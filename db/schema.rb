23f533e227d04fda5f3c1c5d4c0757e68b1ddaaa
Ben Flores <benjaminhyw@gmail.com>
2017-02-01 15:44:08 -0800

    Generate category model, run migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170201224715) do
+ActiveRecord::Schema.define(version: 20170201233759) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -33,7 +33,6 @@ ActiveRecord::Schema.define(version: 20170201224715) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "email"
-    t.string   "password"
     t.boolean  "admin"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
