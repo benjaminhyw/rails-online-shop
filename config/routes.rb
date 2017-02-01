da5bde07ed04bb8b90f3595dbeaf83111be9d322
Ben Flores <benjaminhyw@gmail.com>
2017-02-01 14:05:39 -0800

    Create get /categories/new named url

--- config/routes.rb
+++ config/routes.rb
@@ -25,5 +25,7 @@ Rails.application.routes.draw do
   delete '/items/:id', to: 'items#destroy'
 
   get '/categories', to: 'categories#index'
+  get '/categories/new', to: 'categories#new', as: 'new_category'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
