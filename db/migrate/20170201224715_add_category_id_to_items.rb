692dc5c1bdffac678574032efa5d15ed20476c3c
Ben Flores <benjaminhyw@gmail.com>
2017-02-02 14:49:41 -0800

    Change migrations so arrays have a default value

--- db/migrate/20170201224715_add_category_id_to_items.rb
+++ db/migrate/20170201224715_add_category_id_to_items.rb
@@ -1,5 +1,5 @@
 class AddCategoryIdToItems < ActiveRecord::Migration[5.0]
   def change
-    add_column :items, :category_id, :text, :array => true
+    add_column :items, :category_id, :text, :array => true, :default => '{}'
   end
 end
