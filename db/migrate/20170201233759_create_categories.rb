08e13e18a458a194dae4f9fb8878eea87d2c19aa
Ben Flores <benjaminhyw@gmail.com>
2017-02-02 14:48:40 -0800

    New approach to arrays in database

--- db/migrate/20170201233759_create_categories.rb
+++ db/migrate/20170201233759_create_categories.rb
@@ -2,7 +2,7 @@ class CreateCategories < ActiveRecord::Migration[5.0]
   def change
     create_table :categories do |t|
       t.string :name
-      t.integer :product_id
+      t.text :product_id, :array => true
 
       t.timestamps
     end
