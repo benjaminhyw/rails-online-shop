5c2ed2f8d6c2aa87cc67d3987d28008fe0fc9242
Ben Flores <benjaminhyw@gmail.com>
2017-02-01 16:36:49 -0800

    Create private #find_category and #category_params inside CategoriesHelper

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  private
+    def find_category
+      @category = Category.find_by_id(params[:id])
+    end
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
 end
