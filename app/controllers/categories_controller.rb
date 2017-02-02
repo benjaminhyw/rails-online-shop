482913cdc46150201d74f98d61ad0f797572af79
Ben Flores <benjaminhyw@gmail.com>
2017-02-01 16:41:43 -0800

    Add logic for CategoriesController#create and #show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,4 +11,26 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def create
+    if is_admin?
+      category_params
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to @category
+      else
+        render 'new'
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  def show
+    find_category
+    if !@category
+      redirect_to root_path
+    end
+  end
+
+
 end
