42595bd4c049ea1f126021bf8ff712282d076b9b
Ben Flores <benjaminhyw@gmail.com>
2017-02-05 14:32:11 -0800

    Create private CategoriesHelper#categories and create instance variable @categories to have access throughout entire application

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,9 @@
 module CategoriesHelper
   private
+    def categories
+      @categories = Category.all
+    end
+
     def find_category
       @category = Category.find_by_id(params[:id])
     end
