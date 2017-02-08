f6c9363a2612fbb3b24e422be4b99483399521df
Ben Flores <benjaminhyw@gmail.com>
2017-02-08 15:10:16 -0800

    Create CategoriesHelper#current_item_category and #delete_item_from_old_category

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,5 +1,16 @@
 module CategoriesHelper
   private
+    def current_item_category
+      find_item
+      @category = Category.find_by_id(@item.category_id)
+    end
+
+    def delete_item_from_old_category
+      current_item_category
+      @category.item_id.delete(@item.id)
+      @category.save
+    end
+
     def categories
       @categories = Category.all
     end
