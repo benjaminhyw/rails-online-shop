fc6f9c9bd9c8433dedc84e8b0bcb13f86e02ed72
Ben Flores <benjaminhyw@gmail.com>
2017-02-13 12:28:53 -0800

    include :item_id array inside orders

--- db/migrate/20170209204216_create_orders.rb
+++ db/migrate/20170209204216_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration[5.0]
   def change
     create_table :orders do |t|
       t.integer :user_id
+      t.text :item_id, :array => true, :default => [].to_yaml
 
       t.timestamps
     end
