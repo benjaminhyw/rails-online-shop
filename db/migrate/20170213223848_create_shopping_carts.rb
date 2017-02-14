c56adc65f73c2b1013260de15997940ffa17df47
Ben Flores <benjaminhyw@gmail.com>
2017-02-13 18:34:33 -0800

    Update migrations, run migration

--- db/migrate/20170213223848_create_shopping_carts.rb
+++ db/migrate/20170213223848_create_shopping_carts.rb
@@ -2,6 +2,7 @@ class CreateShoppingCarts < ActiveRecord::Migration[5.0]
   def change
     create_table :shopping_carts do |t|
       t.integer :user_id
+      t.text :item_id, :array => true, :default => [].to_yaml
 
       t.timestamps
     end
--- db/migrate/20170213232724_create_users_and_shopping_cart.rb
+++ db/migrate/20170213232724_create_users_and_shopping_cart.rb
@@ -1,6 +1,6 @@
 class CreateUsersAndShoppingCart < ActiveRecord::Migration[5.0]
   def change
-    create_table :users_shopping_carts do |t|
+    create_table :users_shopping_cart do |t|
       t.belongs_to :user, index: true
       t.belongs_to :shopping_cart, index: true
     end
