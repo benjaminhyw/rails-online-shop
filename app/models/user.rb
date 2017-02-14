38e9b3d09eb22d2c7db2f76ea9db666c613e33f4
Ben Flores <benjaminhyw@gmail.com>
2017-02-14 14:22:09 -0800

    COmment out password validation, because this is what prevents add_to_cart.  Gotta figure out password_digest type of validation instead

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates :username, presence: { message: "Username field can't be left blank"}, length: { in: 4..15, message: 'Username must be between 4-15 characters in length' }, uniqueness: { message: 'Username is already taken'}
   # email validation still needs email format validation
   validates :email, presence: { message: "Email can't be left blank" }, uniqueness: { message: "Email is already taken"}
-  validates :password, presence: { message: "Password field can't be left blank" }, length: { in: 4..10, message: "Password must be between 4-15 characters in length" }
+  # validates :password, presence: { message: "Password field can't be left blank" }, length: { minimum: 4, message: "Password must be between 4-15 characters in length" }
 
   has_secure_password validations: false
   # has_many :orders
