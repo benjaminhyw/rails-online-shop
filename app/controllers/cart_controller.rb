f09447880c64715bf9b2d65c50b93e375319b652
Ben Flores <benjaminhyw@gmail.com>
2017-02-20 15:37:46 -0800

    Create and begin fleshing out CartController#checkout.  Rename from previous CartController#thank_you

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,13 @@
 class CartController < ApplicationController
   def index
   end
+
+  def checkout
+    current_user
+    @current_cart = @current_user.shopping_cart
+    if logged_in? && !@current_cart.empty?
+      empty_cart
+      redirect_to cart_path
+    end
+  end
 end
